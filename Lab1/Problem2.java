e7167c2322fd9dbcd1b3b9b38aaaed6375824bd3
MargulanYe <ezhekhanmargulan@gmail.com>
2026-01-27 15:52:23 +0500

    Create Problem2.java

---
+++ Lab1/Problem2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What's the length of one side?");
+		int a = scanner.nextInt();
+		double b = a;
+		
+		System.out.println("Area: " + a*a);
+		System.out.println("Perimeter: " + 4*a);
+		System.out.println("Diagonal: " + Math.sqrt(2)*b);
+	}
+}
