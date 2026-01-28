e787ba1681a05fe31e66b25e809ee9a0c06db1a1
MargulanYe <ezhekhanmargulan@gmail.com>
2026-01-28 16:13:48 +0500

    Update Problem5.java

--- Lab1/Problem5.java
+++ Lab1/Problem5.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		int balance = 0;
-		System.out.println("Your current balance is: " + balance);
-		System.out.println("Please enter your percentage: ");
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		int money = scanner.nextInt();
-		balance *= money;
-		
-		System.out.println("You inserted " + money + "KZT! Your current balance is " + balance);
-		
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Please enter your initial balance: ");
+        int balance = scanner.nextInt();
+
+        System.out.println("Your current balance is: " + balance);
+
+        System.out.println("How much money do you want to insert?");
+        int money = scanner.nextInt();
+
+        balance *= money;
+
+        System.out.println("You inserted " + money + " KZT! Your new balance is " + balance);
+    }
 }
