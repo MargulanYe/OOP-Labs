afbc0f3a614d7467cdefbf6313bc0a038c6eb134
MargulanYe <ezhekhanmargulan@gmail.com>
2026-02-02 21:38:08 +0500

    Update Problem2.java

--- Lab2/Problem2.java
+++ Lab2/Problem2.java
@@ -1,21 +1,37 @@
-class StarTriangle {
+import java.util.Scanner;
+
+public class StarTriangle {
     private int width;
 
     public StarTriangle(int width) {
         this.width = width;
     }
-
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-
+        StringBuilder result = new StringBuilder();
         for (int i = 1; i <= width; i++) {
-            for (int j = 1; j <= i; j++) {
-                sb.append("[*]");
+            for (int j = 0; j < i; j++) {
+                result.append("[*]");
             }
-            sb.append("\n");
+            if (i < width) {
+                result.append("\n");
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите ширину треугольника: ");
+        if (input.hasNextInt()) {
+            int userWidth = input.nextInt();
+            StarTriangle triangle = new StarTriangle(userWidth);
+            System.out.println("\nВаш треугольник:");
+            System.out.println(triangle.toString());
+        } else {
+            System.out.println("Ошибка: введите целое число.");
         }
 
-        return sb.toString();
+        input.close();
     }
 }
